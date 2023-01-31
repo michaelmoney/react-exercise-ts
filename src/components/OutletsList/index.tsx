a341c1d841045e69cfb2fab3555ba68b296a757c
Michal Pieniazek <michal.pieniazek@wipro.com>
2023-01-31 22:11:38 +0100

    Add instructions

--- src/components/OutletsList/index.tsx
+++ src/components/OutletsList/index.tsx
@@ -1,6 +1,35 @@
 import { useEffect, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 
+/*
+
+1. Display in OutletsList component a list of Food Outlets for the Huston city from page no. 1 by utilizing below API when components
+
+GET request https://jsonmock.hackerrank.com/api/food_outlets?city=Houston&page=1
+returns data associated with city Houston, and on the first page of the results.
+
+Example of the JSON output:
+
+{
+    "city": "Houston",
+    "name": "Cocoa Tree",
+    "estimated_cost": 10,
+    "user_rating": {
+        "average_rating": 4.5,
+        "votes": 969
+    },
+    "id": 938
+},
+
+2. Sort results descending using "average_rating" field (highest score at the top)
+
+3. Add toggle button that allows switching sorting order (ascending/descending)
+
+4. Display in OutletsList component a list of Food Outlets for the Huston city from all available pages
+
+*/
+
+
 interface ApiResponse {
     page: number,
     per_page: number,
