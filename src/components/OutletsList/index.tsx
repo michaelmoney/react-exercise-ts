8c4ead1b46e9d0cde3990dc6ff9870dfae97664c
Michal Pieniazek <michal.pieniazek@wipro.com>
2023-01-31 22:12:20 +0100

    Add instructions

--- src/components/OutletsList/index.tsx
+++ src/components/OutletsList/index.tsx
@@ -1,48 +1,58 @@
-interface ApiResponse {
-    page: number,
-    per_page: number,
-    total: number,
-    total_pages: number,
-    data: Outlets[]
-}
+/*
 
-type Outlets =  {
-    id: string;
-    city: string;
-    name: string;
-    estimated_cost: number;
-    user_rating: { average_rating: number; votes: number}
-}
+1. Display in OutletsList component a list of Food Outlets for the Huston city from page no. 1 by utilizing below API when components
 
+GET request https://jsonmock.hackerrank.com/api/food_outlets?city=Houston&page=1
+returns data associated with city Houston, and on the first page of the results.
+
+Example of the JSON output:
+
+{
+    "city": "Houston",
+    "name": "Cocoa Tree",
+    "estimated_cost": 10,
+    "user_rating": {
+        "average_rating": 4.5,
+        "votes": 969
+    },
+    "id": 938
+},
+
+2. Sort results descending using "average_rating" field (highest score at the top)
+
+3. Add toggle button that allows switching sorting order (ascending/descending)
+
+4. Display in OutletsList component a list of Food Outlets for the Huston city from all available pages
+
+*/
 
 const OutletsList = () => {
-    return <div className="container">
-        <h1>Outlets List</h1>
+    return (
+        <div className="container">
+            <h1>Food Outlets List</h1>
             <table className="table">
                 <thead>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Name</th>
-                    <th
-                        scope="col"
-                    >Score
-                    </th>
+                    <th scope="col">Score</th>
                 </tr>
                 </thead>
                 <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>KFC</td>
-                            <td>3.2</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>McDonald's</td>
-                            <td>4.2</td>
-                        </tr>
+                <tr>
+                    <td>1</td>
+                    <td>KFC</td>
+                    <td>3.2</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>McDonald's</td>
+                    <td>4.2</td>
+                </tr>
                 </tbody>
             </table>
-    </div>
-}
+        </div>
+    );
+};
 
 export default OutletsList;
